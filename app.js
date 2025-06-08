a833ee6c3f5e598f2d021254336a3f705b9a7d3a
duyb2207513 <duyb2207513@student.ctu.edu.vn>
2025-06-08 11:36:04 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require("express");
 const cors =require("cors");
 const app = express();
 
-const contactRouter = require('./app/routes/contact.route');
+const ContactRouter=require("./app/routes/contact.router");
+const ApiError = require("./app/api-error");
 app.use(cors());
 app.use(express.json());
 app.get("/", (req,res) => {
     res.json({message:"Welcome to contact book application"})
 });
-app.use("/api/contacts",contactRouter);
+app.use("/api/contacts",ContactRouter);
 
+
+//Handle 404 response
+app.use((req,res,next)=>{
+    return next(new ApiError(404,"Resource not found"));
+});
+app.use((err,req,res,next)=>{
+    return res.status(error.statusCode||500).json({
+        message:error.message|| "Internal Sever Error",
+    })
+})
 module.exports= app;
